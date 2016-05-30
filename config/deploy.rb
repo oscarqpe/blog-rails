8e1940e3f117ae4c15dfaad8f6115a1034c35607
=Oscar Edmit Quispe Poccohuanca <oscarqc@nspsac.com>
2016-05-30 10:46:49 -0500

    deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'blog'
-# set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'https://github.com/oscarqpe/blog-rails.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
